6a846985a96e46cb271fd0e613bc7d2b8602d5d1
Mikko Korpela <mikko.korpela@gmail.com>
2022-12-20 23:31:30 +0200

    some initial styling

--- src/RobotLog.tsx
+++ src/RobotLog.tsx
@@ -5,35 +5,49 @@ import {
     RobotFrameworkResultSuite,
     RobotFrameworkResultTest
 } from "./Suite";
+import styles from './RobotLog.module.css';
 
 const MessageLog:FC<{message:RobotFrameworkResultMessage}> = ({message}):JSX.Element => {
-    return <div>
-        <span>{message.message}</span>
-        <div>{message.level}</div>
-        <div>{message.timestamp.toISOString()}</div>
+    return <div className={styles.vertical}>
+        <div className={styles.timestamp}>{message.timestamp.toISOString()}</div>
+        <div className={styles.status}>{message.level}</div>
+        <pre className={styles.logmessage}>{message.message}</pre>
     </div>
 }
 
 const KeywordLog:FC<{keyword:RobotFrameworkResultKeyword}> = ({keyword}):JSX.Element => {
-    return <div>
-        <label>{keyword.name}</label>
+    return <div className={styles.container}>
+        <div className={styles.vertical}>
+            <div className={styles.timestamp}>{keyword.times.startTime.toISOString()}</div>
+            <div className={styles.status}>{keyword.status}</div>
+            <span className={styles.logmessage}>{keyword.type}: {keyword.name} : {keyword.args}</span>
+        </div>
         {keyword.keywords.length > 0 && (<div>{keyword.keywords.map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>)}
         <div>{keyword.messages.map((m, i) => <MessageLog key={i} message={m}/>)}</div>
     </div>
 }
 
 const TestLog:FC<{test:RobotFrameworkResultTest}> = ({test}):JSX.Element => {
-    return <div>
-        <label>{test.name}</label>
+    return <div className={styles.container}>
+        <div className={styles.vertical}>
+            <div className={styles.timestamp}>{test.times.startTime.toISOString()}</div>
+            <div className={styles.status}>{test.status}</div>
+            <span className={styles.logmessage}>TEST: {test.name}</span>
+        </div>
         <div>{test.keywords.map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
     </div>
 }
 
 export const RobotLog:FC<{suite:RobotFrameworkResultSuite}> = ({suite}):JSX.Element => {
-    return <div>
-        <label>{suite.name}</label>
-        <div>{suite.keywords.map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
+    return <div className={styles.container}>
+        <div className={styles.vertical}>
+            <div className={styles.timestamp}>{suite.times.startTime.toISOString()}</div>
+            <div className={styles.status}>{suite.status}</div>
+            <span className={styles.logmessage}>SUITE: {suite.name}</span>
+        </div>
+        <div>{suite.keywords.filter(k => k.type === 'SETUP').map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
         <div>{suite.tests.map(t => <TestLog key={t.id} test={t}/>)}</div>
         <div>{suite.suites.map(s => <RobotLog key={s.id} suite={s}/>)}</div>
+        <div>{suite.keywords.filter(k => k.type === 'TEARDOWN').map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
     </div>
 }
