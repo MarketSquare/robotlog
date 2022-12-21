f26f7489ee6445defa8c4cfbf28695b080190583
Mikko Korpela <mikko.korpela@gmail.com>
2022-12-21 20:28:30 +0200

    stickyness

--- src/RobotLog.tsx
+++ src/RobotLog.tsx
@@ -15,39 +15,51 @@ const MessageLog:FC<{message:RobotFrameworkResultMessage}> = ({message}):JSX.Ele
     </div>
 }
 
-const KeywordLog:FC<{keyword:RobotFrameworkResultKeyword}> = ({keyword}):JSX.Element => {
+const KeywordLog:FC<{keyword:RobotFrameworkResultKeyword, level:number}> = ({keyword, level}):JSX.Element => {
     return <div className={styles.container}>
-        <div className={`${styles.vertical} ${styles.keywordheader}`}>
+        <div className={`${styles.vertical} ${styles.sticker}`}
+        style={{top: 19*level, zIndex: 1000 - 100*level}}
+        >
             <div className={styles.timestamp}>{keyword.times.startTime.toISOString()}</div>
+            <div className={styles.status}>{keyword.type}</div>
+            <span className={styles.logmessage}
+            >
+                {keyword.name} : {keyword.args}</span>
             <div className={styles.status}>{keyword.status}</div>
-            <span className={styles.logmessage}>{keyword.type}: {keyword.name} : {keyword.args}</span>
         </div>
-        {keyword.keywords.length > 0 && (<div>{keyword.keywords.map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>)}
+        {keyword.keywords.length > 0 && (<div>{keyword.keywords.map(k => <KeywordLog key={k.id} keyword={k} level={level+1}/>)}</div>)}
         <div>{keyword.messages.map((m, i) => <MessageLog key={i} message={m}/>)}</div>
     </div>
 }
 
-const TestLog:FC<{test:RobotFrameworkResultTest}> = ({test}):JSX.Element => {
+const TestLog:FC<{test:RobotFrameworkResultTest, level:number}> = ({test, level}):JSX.Element => {
     return <div className={styles.container}>
-        <div className={`${styles.vertical} ${styles.testheader}`}>
+        <div className={`${styles.vertical} ${styles.sticker}`}
+        style={{top: 19*level, zIndex: 1000 - 100*level}}
+        >
             <div className={styles.timestamp}>{test.times.startTime.toISOString()}</div>
+            <div className={styles.status}>TEST</div>
+            <span className={styles.logmessage}
+            >: {test.name}</span>
             <div className={styles.status}>{test.status}</div>
-            <span className={styles.logmessage}>TEST: {test.name}</span>
         </div>
-        <div>{test.keywords.map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
+        <div>{test.keywords.map(k => <KeywordLog key={k.id} keyword={k} level={level+1}/>)}</div>
     </div>
 }
 
-export const RobotLog:FC<{suite:RobotFrameworkResultSuite}> = ({suite}):JSX.Element => {
+export const RobotLog:FC<{suite:RobotFrameworkResultSuite, level?:number}> = ({suite, level=0}):JSX.Element => {
     return <div className={styles.container}>
-        <div className={`${styles.vertical} ${styles.suiteheader}`}>
+        <div className={`${styles.vertical} ${styles.sticker}`}
+        style={{top: 19*level, zIndex: 1000 - 100*level}}
+        >
             <div className={styles.timestamp}>{suite.times.startTime.toISOString()}</div>
+            <div className={styles.status}>SUITE</div>
+            <span className={styles.logmessage}>{suite.name}</span>
             <div className={styles.status}>{suite.status}</div>
-            <span className={styles.logmessage}>SUITE: {suite.name}</span>
         </div>
-        <div>{suite.keywords.filter(k => k.type === 'SETUP').map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
-        <div>{suite.tests.map(t => <TestLog key={t.id} test={t}/>)}</div>
-        <div>{suite.suites.map(s => <RobotLog key={s.id} suite={s}/>)}</div>
-        <div>{suite.keywords.filter(k => k.type === 'TEARDOWN').map(k => <KeywordLog key={k.id} keyword={k}/>)}</div>
+        <div>{suite.keywords.filter(k => k.type === 'SETUP').map(k => <KeywordLog key={k.id} keyword={k} level={level+1}/>)}</div>
+        <div>{suite.tests.map(t => <TestLog key={t.id} test={t} level={level+1}/>)}</div>
+        <div>{suite.suites.map(s => <RobotLog key={s.id} suite={s} level={level+1}/>)}</div>
+        <div>{suite.keywords.filter(k => k.type === 'TEARDOWN').map(k => <KeywordLog key={k.id} keyword={k} level={level+1}/>)}</div>
     </div>
 }
